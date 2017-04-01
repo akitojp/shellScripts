091f4910857d49e57e8548be5780e75d0d5432c0
Akito Tsutsui <akito.tsutsui@gmail.com>
2017-04-01 18:34:41 +0900

    Create mariadb_install.sh

---
+++ mariadb_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo yum install -y mariadb mariadb-server
+
+sudo cp -p /usr/share/mysql/my-small.cnf /etc/my.cnf.d/server.cnf
+sudo sed -i -e "/^\[mysqld\]$/a character-set-server=utf8" /etc/my.cnf.d/server.cnf
+sudo sed -i -e "/^\[mysqld\]$/a character-set-server=utf8" /etc/my.cnf.d/server.cnf
+
+sudo systemctl enable mariadb.service
+sudo systemctl start mariadb.service
+
+# sudo mysql_secure_installation
+# mysql -u root -p
+# show databases;
+# CREATE DATABASE test01_db DEFAULT CHARACTER SET utf8;
+# hogeユーザーを作成
+# MariaDB [(none)]> CREATE USER 'USERNAME'@'localhost' IDENTIFIED BY 'password';
+# テスト用のデータベース（test01_db）に権限があるユーザー（hoge）を設定
+# MariaDB [(none)]> GRANT ALL PRIVILEGES ON test01_db.* TO 'USERNAME'@'localhost' IDENTIFIED BY 'password';
